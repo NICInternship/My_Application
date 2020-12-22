d3a68c342107302915df49f15c284c54b7e26031
NIC1 <tnrdmobile@gmail.com>
2020-12-22 16:30:18 +0530

    added textbox, edittext, and button.

--- app/src/main/java/com/nic/myapplication/MainActivity.java
+++ app/src/main/java/com/nic/myapplication/MainActivity.java
@@ -3,12 +3,31 @@ package com.nic.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText input;
+    TextView label,value;
+    Button submit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        input=findViewById(R.id.input);
+        label=findViewById(R.id.label);
+        value=findViewById(R.id.value);
+        submit=findViewById(R.id.submit);
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                value.setText(input.getText().toString());
+            }
+        });
     }
 }
